d632d865da89c25eee67074cb3bfd3c00e6a251c
Jacoby Ismail <ismail.jacoby@gmail.com>
2024-08-09 16:46:22 +0200

    functions with inputs

--- Python Basics/6.functions.py
+++ Python Basics/6.functions.py
@@ -9,10 +9,17 @@ def say_hello_world():
 # Call the function to execute its code
 say_hello_world()
 
-# Define a function (with parameter) that adds two numbers
+# Functions that have inputs, in this case (a,b)
 def add_two_numbers(a, b):
   return a + b
 
 # Call the function with arguments 10 and 5, and print the result
 result = add_two_numbers(10, 5)
 print(result)  # Output: 15
+
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}")
+
+# Keyword argument
+greet_with(name="John", location="Luxembourg")
